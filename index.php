76c41fa452601480e8de412f5944ab0e566f3dcb
Ndongba01 <ndongba01@gmail.com>
2024-04-02 13:25:53 +0000

    integration base de données

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<?php
+include 'models/DB.php';
+
+?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
